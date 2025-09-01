d9c85d2560a9a6197ea250c49db8ce445eb91e7b
Arush-Developer <arushg.official@gmail.com>
2025-09-01 23:03:50 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="py-6 px-6">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
-        <div style={{ fontFamily: 'Dancing Script, cursive' }} className="text-2xl text-rose-600">For Disha Jain</div>
+        <div style={{ fontFamily: 'Dancing Script, cursive' }} className="text-2xl text-rose-600">I love you Bacha</div>
         <nav>
           <a className="text-sm text-gray-600 mr-4" href="#intro">Intro</a>
           <a className="text-sm text-gray-600 mr-4" href="#fun">Fun</a>
