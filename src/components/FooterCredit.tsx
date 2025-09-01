51133643bcc6a1bf6c0e406724f759c653c58171
Arush-Developer <arushg.official@gmail.com>
2025-09-01 22:45:11 +0530

    Create FooterCredit.tsx

---
+++ src/components/FooterCredit.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FooterCredit() {
+  return (
+    <footer className="py-12 bg-gradient-to-br from-pink-50 to-roseGold/10">
+      <div className="max-w-4xl mx-auto text-center text-gray-700">
+        <div style={{ fontFamily: 'Dancing Script, cursive' }} className="text-rose-600 text-xl">To many more years</div>
+        <div className="mt-3">Written with love by Sahil Aggarwal</div>
+      </div>
+    </footer>
+  )
+}
